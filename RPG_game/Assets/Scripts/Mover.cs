3de9807abca36477ed420539474e3b4b600cee1a
Mehmet Said ÖZSOY <mehmetsaidozsoy@gmail.com>
2021-05-02 05:37:01 +0300

    Nav Mesh Agent ile ilk hareket:)

---
+++ RPG_game/Assets/Scripts/Mover.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+
+    [SerializeField] Transform target;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<NavMeshAgent>().destination = target.position;
+
+    }
+}
